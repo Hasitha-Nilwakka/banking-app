6506c46b096310538204b9bcffb10c15e37bd10e
Hasitha-Nilwakka <hasitha.nilwakka@yahoo.com>
2026-04-06 11:56:36 +0300

    initiating project from begining with TS + react for front end

--- client/src/utils/navStyles.js
+++
@@ -1,7 +0,0 @@
-import theme from "@/constants/theme"
-export const navStyles = ({isActive}) => ({
-                    color : isActive ? theme.colors.primaryColor : theme.colors.secondaryColor,
-                    fontWeight : isActive ? 'bold' : 'normal'
-                })
-
-export default navStyles
